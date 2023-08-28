777785c167e332ecccd3ea929252ffb391939201
Svenja737 <filthaut@cl.uni-heidelberg.de>
2023-08-28 22:36:34 +0200

    add wandb api login

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,36 +1,14 @@
-import pytorch_lightning as pl
-from data_libs.sd_datamodule import SpeakerClassificationDataModule
-from modeling.sd_classification import SpeakerDiarizationCorrectionModule
-from pytorch_lightning.loggers import WandbLogger
+from sd_error_correction import SDErrorCorrectionPipeline
 import os
 import wandb
-from transformers import RobertaConfig
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 def main():
 
-    sdm = SpeakerClassificationDataModule(model_name_or_path="roberta-base")
-    sdm.setup("fit")
-    sdm.setup("validate")
-
-    model = SpeakerDiarizationCorrectionModule(
-        model_name_or_path="roberta-base",
-        num_labels=3
-    )
-
-    logger = WandbLogger(save_dir="results/wanddb_logging")
-
-    trainer = pl.Trainer(
-        accelerator="auto",
-        devices="auto",
-        use_distributed_sampler=False,
-        log_every_n_steps=10,
-        enable_progress_bar=True,
-        logger=logger,
-    )
-
-    trainer.fit(model, train_dataloaders=sdm.train_dataloader(), val_dataloaders=sdm.val_dataloader())
+    wandb.login("fa9a8ca94055c30f4a506593d5ed4d3a3cad70f2")
+    sdcp = SDErrorCorrectionPipeline()
+    sdcp.train_model("roberta-base", num_labels=3)
 
 if __name__ == "__main__":
     main()
