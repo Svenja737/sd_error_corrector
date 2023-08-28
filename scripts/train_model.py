7414d5f6e69d5ab607f1bf0c32e71059a9373b8f
Svenja737 <filthaut@cl.uni-heidelberg.de>
2023-08-28 22:53:11 +0200

    wandb config

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -6,7 +6,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 def main():
 
-    wandb.login("fa9a8ca94055c30f4a506593d5ed4d3a3cad70f2")
+    wandb.login(anonymous="allow", key="fa9a8ca94055c30f4a506593d5ed4d3a3cad70f2")
     sdcp = SDErrorCorrectionPipeline()
     sdcp.train_model("roberta-base", num_labels=3)
 
